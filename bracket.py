1f4c866edc3a78a72784f84d8c43e639bc80b528
PranavMish30 <pranavmish30@gmail.com>
2024-12-28 18:54:49 +0530

    Token RegEx mapped.

--- bracket.py
+++ bracket.py
@@ -1,3 +1,4 @@
+import re
 class Token:
     def __init__(self,value,token_type):
         self.type = token_type
@@ -6,13 +7,22 @@ class Token:
 class Lexer:
     def __init__(self,text):    
         self.text = text
-        self.token_types = {}
+        self.token_types = {'KEYWORD': r'\b(if|elif|else|for|while|return)\b' ,
+                            'IDENTIFIER': r'^[a-zA-Z]\w*' ,
+                            'INT': r'\b-?\d+\b' ,
+                            'FLOAT': r'\b-?\d+\.\d+\b' ,
+                            'STR': r'\b"[^"]*"\b' ,
+                            'BOOL': r'\b(True|False)\b' ,
+                            'OPERATOR': r'\b[+\-*/%]\b' ,
+                            'COMMENT': r'\b//.*' ,
+                            'WHITESPACE': r'\s+'
+                            }
         self.tokens = []
     
-    def makeTokens(self):
+    def generateTokens(self):
         return self.tokens
 
 def run(text):
     lexer = Lexer(text)
-    tokens = lexer.makeTokens()
+    tokens = lexer.generateTokens()
     return tokens 
